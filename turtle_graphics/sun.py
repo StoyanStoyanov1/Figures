a66bd0495bab9e0c7b3f2130495dad659518a08c
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-04-27 14:40:08 +0200

    sun

---
+++ turtle_graphics/sun.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+my_turtle = Turtle()
+my_turtle.shape("turtle")
+my_turtle.color('green')
+
+for i in range(1, 36 + 1):
+    my_turtle.left(190)
+    my_turtle.forward(300)
